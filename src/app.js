64f79458251314223c950a65d86cffba66e45c02
i-lema <161314957+i-lema@users.noreply.github.com>
2024-04-24 16:44:00 +0000

    Primeras modificaciones

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = [
+    "Mi perro",
+    "Mi gato",
+    "Un cocodrilo",
+    "Un fantasma",
+    "Un ladrón",
+    "Un dragón"
+  ];
+  let accion = [
+    "robó",
+    "se comió",
+    "destruyó",
+    "quemó",
+    "se fue con",
+    "se llevó"
+  ];
+  let que = [
+    "mis deberes",
+    "mi proyecto",
+    "mi ordenador",
+    "mi coche",
+    "mi móvil",
+    "mi mochila"
+  ];
+  let cuando = [
+    "mientras estaba en clase",
+    "mientras estaba en el trabajo",
+    "mientras daba un paseo",
+    "cuando estaba en la ducha",
+    "mientras estaba comprando",
+    "cuando estaba limpiando"
+  ];
+  function generadorDeExcusas(quien, accion, que, cuando) {
+    let elegirQuien = Math.floor(Math.random() * quien.length);
+    let elegirAccion = Math.floor(Math.random() * accion.length);
+    let elegirQue = Math.floor(Math.random() * que.length);
+    let elegirCuando = Math.floor(Math.random() * cuando.length);
+
+    return `${quien[elegirQuien]} ${accion[elegirAccion]} ${que[elegirQue]} ${cuando[elegirCuando]}`;
+  }
+  let excusa = generadorDeExcusas(quien, accion, que, cuando);
+  document.querySelector("#excuse").textContent = excusa;
 };
